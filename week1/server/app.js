649cf2d2dc6c0b9b5aca54dcca4ebfe17146e7f4
Dylan Lavimodiere <dylanlavimodiere@gmail.com>
2021-01-13 12:59:39 -0500

    2nd commit

--- week1/server/app.js
+++ week1/server/app.js
@@ -1,7 +1,7 @@
 //app is entry point of application
 
 function sendMsg(){
-    console.log("Wouldn't you like to be a pepper too???");//hello world program
+    console.log("Hellow world???");//hello world program
 }
 
 sendMsg();
