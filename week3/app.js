3815b36f810e07f4fd54aa6da6ae8461d4e35c08
DRLdestroyer <dylanlavimodiere@gmail.com>
2021-01-30 01:31:36 -0500

    synced with Jordan's repo

--- week3/app.js
+++ week3/app.js
@@ -8,26 +8,103 @@ app.get('/', function(req,res){
     res.sendFile(__dirname+'/client/index.html');
 });//connection to index
 
-app.use('/client', express.static(__dirname+'/client'))//set as staic folder
+app.use('/client', express.static(__dirname+'/client'))//set as static folder
 
+//server side communication
 serv.listen(3000, function(){
     console.log('Connected on localhost 3000');
 
 })//listen to port 3000
 
-//server side communication
-io.sockets.on('connection', function(socket){//when connected to socket.io, is opened when someone on clinet connects to server
+var SocketList = {};
+var PlayerList = {};
+
+var Player = function(id){//class, similar to constructor
+    var self = {//induvidual instance, holds params
+        x:400,
+        y:300,
+        id:id,
+        number:Math.floor(Math.random()*10),
+        right:false,
+        left:false,
+        up:false,
+        down:false,
+        speed:10
+    }
+    self.updatePosition = function(){
+        //console.log(self.up)
+        if (self.right)
+            self.x += self.speed;
+        if (self.left)
+            self.x -= self.speed;
+        if (self.up)
+            self.y -= self.speed;
+        if (self.down)
+            self.y += self.speed;
+    }
+    return self;
+}
+
+io.sockets.on('connection', function(socket){//when connected to socket.io, is opened when someone on client connects to server
     console.log("Socket Connected");
 
-    socket.on('sendMsg', function(data){//on this event happening
-        console.log(data.message);
-    })
-    socket.on('sendBtnMsg',function(data){
-        console.log(data.message);
-    })
+    socket.id = Math.random();//random id, unlikely to be the same
+    //socket.x = 0;
+    //socket.y = Math.floor(Math.random()*600)//random number * canvas height
+    //socket.number = Math.floor(Math.random()*10)//random number * canvas height
+    //add something to socket list
+    SocketList[socket.id] = socket;
 
-    socket.emit('messageFromServer', {
-        message:'Hey jordan welcome to the party'
-    })
+    var player = new Player(socket.id);//instance of player, utlize new x,y
+    PlayerList[socket.id] = player;
 
+    //disconnection event
+    socket.on('disconnect', function(){
+        delete SocketList[socket.id];
+        delete PlayerList[socket.id];
+    });//disconnection
+
+    //recieves player input
+    socket.on('keypress', function(data){
+        console.log(data.state)
+        if(data.inputId === 'up')
+            player.up = data.state;
+        if(data.inputId === 'down')
+            player.down = data.state;
+        if(data.inputId === 'left')
+            player.left = data.state;
+        if(data.inputId === 'right')
+            player.right = data.state;
+    });
+
+    //old example from wendsday 1/27
+    // socket.on('sendMsg', function(data){//on this event happening
+    //     console.log(data.message);
+    // })
+    // socket.on('sendBtnMsg',function(data){//recive action
+    //     console.log(data.message);
+    // })
+
+    // socket.emit('messageFromServer', {//send out action
+    //     message:'Hey jordan welcome to the party'
+    // })
 });
+
+//setup update loop
+setInterval(function(){
+    var pack = [];//collection of each package
+    for (var i in PlayerList){
+        var player = PlayerList[i];
+        player.updatePosition();
+        //console.log(player)
+        pack.push({
+            x: player.x,
+            y: player.y,
+            number: player.number
+        })
+    }
+    for (var i in SocketList){
+        var socket = SocketList[i];//new local refernce to update previous versions
+        socket.emit('newPositions', pack);
+    }
+}, 1000/30);//code to be executed when run, time in ms
---
+++ week3/client/js/game.js
@@ -0,0 +1,51 @@
+var socket = io();
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+ctx.font = '30px Arial';
+
+//event listeners for keypresses
+document.addEventListener('keydown', keyPressDown)//keydown
+document.addEventListener('keyup', keyPressUp)//key up
+
+function keyPressDown(e) {//recieve event from event handler
+    if (e.keyCode === 39)//right
+        socket.emit('keypress', { inputId: 'right', state: true })
+    else if (e.keyCode === 37)//left
+        socket.emit('keypress', { inputId: 'left', state: true })
+    else if (e.keyCode === 38)//up
+        socket.emit('keypress', { inputId: 'up', state: true })
+    else if (e.keyCode === 40)//down//diagonal movement from else ifs
+        socket.emit('keypress', { inputId: 'down', state: true })
+}
+
+function keyPressUp(e) {//recieve event from event handler
+    if (e.keyCode === 38)//up//diagonal movement from else ifs
+        socket.emit('keypress', { inputId: 'up', state: false })
+    else if (e.keyCode === 40)//down
+        socket.emit('keypress', { inputId: 'down', state: false })
+    else if (e.keyCode === 37)//left
+        socket.emit('keypress', { inputId: 'left', state: false })
+    else if (e.keyCode === 39)//right
+        socket.emit('keypress', { inputId: 'right', state: false })
+}
+
+socket.on('newPositions', function (data) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < data.length; i++) {//data = new package we are sending
+        ctx.fillText(data[i].number, data[i].x, data[i].y);
+    }
+})
+
+        //example code from wendsday 1/27
+        // var msg = function(){//send data through json object to server?
+        //     socket.emit('sendMsg',{
+        //         message: 'Sending Message from button'
+        //     })
+        // }
+        // socket.emit('sendMsg',{//send data through json object to server?//emit a custom envent to our server
+        //         message: 'Hello Jordan I am logged in'
+        // })
+
+        // socket.on('messageFromServer', function(data){
+        //     console.log(data.message);
+        // });
